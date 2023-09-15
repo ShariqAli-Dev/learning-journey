a5ec9bd59f2b66a756fe16dead0b83f80f727257
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-09-15 16:27:14 -0400

    stripe integration with sdk to purchase course

--- src/components/SubscriptionAction.tsx
+++ src/components/SubscriptionAction.tsx
@@ -3,11 +3,23 @@ import { useSession } from "next-auth/react";
 import { Progress } from "./ui/progress";
 import { Button } from "./ui/button";
 import { Zap } from "lucide-react";
-
-interface Props {}
+import { useState } from "react";
+import axios from "axios";
 
 export default function SubscriptionsAction() {
   const { data } = useSession();
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubscribe() {
+    setLoading(true);
+    try {
+      const { data } = await axios.get("/api/stripe");
+      window.location.href = data.url;
+    } catch (err) {
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary">
@@ -16,7 +28,11 @@ export default function SubscriptionsAction() {
         className="mt-2"
         value={data?.user.credits ? data.user.credits * 10 : 0}
       />
-      <Button className="mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600">
+      <Button
+        onClick={handleSubscribe}
+        className="mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600"
+        disabled={loading}
+      >
         Upgrade
         <Zap className="ml-2 fill-white" />
       </Button>
